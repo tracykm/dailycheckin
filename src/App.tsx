c312795837ea9afff4dcae654b133efe9363adc5
Tracy Mullen <tracykmullen@gmail.com>
2023-01-07 09:34:45 -0600

    extra options

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import FeelingImg from "./FeelingImg";
 import process from "immer";
 
-type Entry = { feelings?: number; date: Date };
+type Entry = {
+  feelings?: number;
+  extras?: Record<string, boolean>;
+  date: Date;
+};
 type DateType = { history: Entry[]; currentIdx: number };
 
 function getDays(date1: Date, date2: Date) {
@@ -41,14 +45,42 @@ function getSavedHistory() {
 
 const defaultHistory = getSavedHistory();
 
+const EXTRA_OPTIONS = {
+  social: [
+    "family",
+    "lover",
+    "kids",
+    "friends",
+    "coworkers",
+    "acquaintances",
+    "compliments",
+  ],
+  other: [
+    "great_work",
+    "bad_work",
+    "cook",
+    "clean",
+    "create_art",
+    "create_something",
+  ],
+  vibe: ["relaxed", "excited", "tired", "stressed", "anxious", "annoyed"],
+} as const;
+
 function App() {
   const [data, setData] = useState<DateType>(defaultHistory);
+  console.log(data);
   const today = data.history[data.currentIdx];
   const updateToday = (newData: Partial<Entry>) => {
     const updatedData = process(data, (draft) => {
       draft.history[data.currentIdx].feelings =
         newData.feelings ?? today.feelings;
       draft.history[data.currentIdx].date = newData.date ?? today.date;
+      if (newData.extras) {
+        draft.history[data.currentIdx].extras = today.extras || {};
+        const key = Object.keys(newData.extras)[0];
+        (draft.history[data.currentIdx].extras as any)[key] =
+          newData.extras[key];
+      }
     });
     setData(updatedData);
     localStorage.setItem("history", JSON.stringify(updatedData.history));
@@ -86,6 +118,29 @@ function App() {
             />
           ))}
         </div>
+
+        {Object.values(EXTRA_OPTIONS).map((arr) => (
+          <div
+            style={{
+              display: "flex",
+              gap: 20,
+              cursor: "pointer",
+              paddingTop: 60,
+            }}
+          >
+            {arr.map((d) => (
+              <div
+                onClick={() =>
+                  updateToday({ extras: { [d]: !today?.extras?.[d] } })
+                }
+                className="button"
+                style={{ opacity: today.extras?.[d] ? 1 : "" }}
+              >
+                {d.replace("_", " ")}
+              </div>
+            ))}
+          </div>
+        ))}
       </header>
     </div>
   );
